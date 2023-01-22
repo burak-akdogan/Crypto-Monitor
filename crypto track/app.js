e26c61457b7ff57e04a2b44661045e89f86ebea1
Burak Akdogan <zeliha.bekcan@gmail.com>
2023-01-22 21:57:43 +0300

    calculate

--- crypto track/app.js
+++ crypto track/app.js
@@ -1,7 +1,7 @@
 var btc = document.getElementById("btc");
-var bnb = document.getElementById("bnb");
+// var bnb = document.getElementById("bnb");
 var eth = document.getElementById("eth");
-var avax = document.getElementById("avax");
+// var avax = document.getElementById("avax");
 
 let liveprice={
     "async":true,
@@ -16,9 +16,69 @@ let liveprice={
 $.ajax(liveprice).done(function (response){
     console.log(response);
     btc.innerHTML = response.bitcoin.usd;
-    bnb.innerHTML = response.binancecoin.usd;
+    // bnb.innerHTML = response.binancecoin.usd;
     eth.innerHTML = response.ethereum.usd;
-    dent.innerHTML = response.dent.usd;
+    // dent.innerHTML = response.dent.usd;
 
 
 });
+ //calculate
+const cryptoInput =document.querySelector('#crytovalue')
+const fiatInput = document.querySelector('#fiatvalue')
+const crytoselect = document.querySelector('#crytoselect')
+const fiatselect = document.querySelector('#fiatselect')
+const fiat = [
+{
+currency: "USD",
+country: "United States Dollar"
+},
+] 
+const coins = [
+{
+currency: "BTC",
+country: "Bitcoin"
+},
+
+]
+
+insertCurrencies(coins,crytoselect)
+insertCurrencies(fiat,fiatselect) 
+
+function insertCurrencies(curr, selectEl){
+  curr.forEach(c=>{
+    selectEl.appendChild(currOption(c.currency,c.country))
+  })
+  function currOption(value,title){
+    const o=document.createElement('option')
+    o.title = title
+    o.value = value
+    o.text = value
+    o.className = 'calcoption'
+    return o
+  }
+}
+
+calc('crypto')
+fiatInput.onkeyup = ()=>calc()
+fiatInput.onchange = ()=>calc()
+cryptoInput.onkeyup = ()=>calc('crypto')
+cryptoInput.onchange = ()=>calc('crypto')
+
+fiatselect.onchange = ()=>calc('crypto')
+crytoselect.onchange = ()=>calc()
+
+async function calc(changer){
+  const fiat = fiatselect.value
+  const cryptoVal = Number(cryptoInput.value)
+  const fiatVal = Number(fiatInput.value) 
+  const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${fiat.toLowerCase()}.json`)
+  const json = await res.json() 
+  const exchangeRate = parseInt(json.bpi[fiat].rate_float)
+  if(changer==='crypto'){
+    const amount =  cryptoVal * exchangeRate 
+    fiatInput.value = parseFloat(amount).toFixed(2)
+  }else{
+    const amount =  fiatVal / exchangeRate 
+    cryptoInput.value = amount
+  } 
+}
